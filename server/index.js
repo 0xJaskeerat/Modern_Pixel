b7324646553a0a1be316ac67c144f3533f8adb7d
Jaskeerat Singh <jaskeerat10000@gmail.com>
2023-08-04 23:38:52 +0530

    fix: 🍄 updated middleware code to enable access from different origin

--- server/index.js
+++ server/index.js
@@ -11,22 +11,19 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// middlewares
-const allowedOrigins = ['https://modern-pixel-frontend.vercel.app/', 'https://modern-pixel.onrender.com'];
+app.use(function (req, res, next) {
+  const allowedOrigins = ['https://modern-pixel-frontend.vercel.app/', 'https://modern-pixel.onrender.com/'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  next();
+});
 
-app.use(
-    cors({
-      origin:  function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      methods: ["GET", "POST"],
-    })
-  );
-app.use(express.json({ limit: '50mb'}))
+app.use(express.json({ limit: '50mb' }))
 
 //routes
 app.use('/api/v1/post', postRoutes);
@@ -39,12 +36,12 @@ app.get('/', async (req, res) => {
 });
 
 const startServer = async () => {
-    try {
-      connectDB(process.env.MONGODB_URL);
-      app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  startServer();
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
